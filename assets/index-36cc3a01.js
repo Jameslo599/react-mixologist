bcd92c8b9570708213b862e266fbcc38f6393e5c
Jameslo599 <Jameslo599@users.noreply.github.com>
2024-11-19 12:05:02 +0000

    deploy: e11fc3048af644ed6fc4c064901b1e970736d09e

--- assets/index-36cc3a01.js
+++ assets/index-36cc3a01.js
@@ -148,7 +148,7 @@ Error generating stack: `+o.message+`
     color: var(--grey-500);
     margin-top: 2rem;
   }
-`,jC=()=>Ce(UC,{children:[$("h3",{children:"About Us"}),$("p",{children:`Introducing "MixMaster," the ultimate party sidekick app that fetches cocktails from the hilarious Cocktails DB API. With a flick of your finger, you'll unlock a treasure trove of enchanting drink recipes that'll make your taste buds dance and your friends jump with joy. Get ready to shake up your mixology game, one fantastical mocktail at a time, and let the laughter and giggles flow!`})]}),BC=mr.div`
+`,jC=()=>Ce(UC,{children:[$("h3",{children:"About Us"}),$("p",{children:`Introducing "MixMaster," the ultimate party sidekick app that fetches cocktails from the hilarious Cocktails DB API. With a flick of your finger, you'll unlock a treasure trove of enchanting drink recipes that'll make your taste buds dance and your friends jump with joy. Get ready to shake up your mixology game, one fantastical mocktail at a time, and let the laughter and giggles flow! Hello World!`})]}),BC=mr.div`
   header {
     text-align: center;
     margin-bottom: 3rem;
